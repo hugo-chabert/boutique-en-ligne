8b85405f6f15f59cbeb5d5769522837e192046f7
francois-niang1 <francois.niang@laplateforme.io>
2022-02-22 15:15:52 +0100

    header index + footer index almost done

--- view/story.php
+++ view/story.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <link rel="stylesheet" href="../public/css/story.css">
+    <link rel="stylesheet" href="../public/css/root&font.css">
+    <title>Qui sommes-nous ?</title>
 </head>
 <body>
-    <?php require("view/header_index.php");?>
+    <?php require("../public/")?>
     <main>
-        
+
     </main>
-    <?php require("view/footer_index.php");?>
+    <?php require("../public/")?>
 </body>
 </html>
