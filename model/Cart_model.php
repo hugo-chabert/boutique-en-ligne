3a30edea87f3f0344e1ab712f8d9c545147f1001
Hugo <hugo.chabert@laplateforme.io>
2022-03-10 11:13:46 +0100

    Displaying carts 50%

--- model/Cart_model.php
+++ model/Cart_model.php
@@ -4,16 +4,30 @@ require_once(__DIR__ . '/../database/database.php');
 
 class Cart_model{
 
-
-    public function sql_add_to_cart($id_user, $id_item){
-        $req = "INSERT INTO carts (id_user, id_item) VALUE (:id_user, :id_item)";
+    public function check_if_multiple_item($id_user, $id_item){
+        $req = "SELECT * FROM carts WHERE id_user = :id_user AND id_item = :id_item";
         $stmt = Database::connect_db()->prepare($req);
         $stmt->execute(array(
-            ':id_user' => $id_user,
-            ':id_item' => $id_item
+            ":id_user" => $id_user,
+            ":id_item" => $id_item
         ));
-        header("Location: cart.php");
-        exit();
+        return $results;
+    }
+
+    public function sql_add_to_cart($id_user, $id_item){
+        if(check_if_multiple_item($id_user, $id_item) == true){
+            echo 'oui';
+        }
+        else{
+            $req = "INSERT INTO carts (id_user, id_item) VALUE (:id_user, :id_item)";
+            $stmt = Database::connect_db()->prepare($req);
+            $stmt->execute(array(
+                ':id_user' => $id_user,
+                ':id_item' => $id_item
+            ));
+            header("Location: cart.php");
+            exit();
+        }
     }
 }
 ?>
--- view/cart.php
+++ view/cart.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+require_once(__DIR__.'/../controller/Cart.php');
 require_once(__DIR__ . '/../controller/Toolbox.php');
 require_once(__DIR__ . '/../controller/Security.php');
 
@@ -18,7 +19,10 @@ if(!Security::isConnect()){
 <body>
     <?php require('header.php');?>
     <main>
-
+        <form action="" method="post">
+        <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
+        <label for="subscribeNews">Souhaitez-vous vous abonner à la newsletter ?</label>
+        </form>
     </main>
     <?php require('footer.php');?>
 </body>
