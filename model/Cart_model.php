ea9a409b8eb8ae76c408f85a91725c72bcf313ba
Hugo <hugo.chabert@laplateforme.io>
2022-03-10 11:52:57 +0100

    Adding quantity in cart

--- model/Cart_model.php
+++ model/Cart_model.php
@@ -4,22 +4,30 @@ require_once(__DIR__ . '/../database/database.php');
 
 class Cart_model{
 
-    public function check_if_multiple_item($id_user, $id_item){
+    public function sql_add_to_cart($id_user, $id_item){
         $req = "SELECT * FROM carts WHERE id_user = :id_user AND id_item = :id_item";
         $stmt = Database::connect_db()->prepare($req);
         $stmt->execute(array(
             ":id_user" => $id_user,
             ":id_item" => $id_item
         ));
-        return $results;
-    }
-
-    public function sql_add_to_cart($id_user, $id_item){
-        if(check_if_multiple_item($id_user, $id_item) == true){
-            echo 'oui';
+        $row = $stmt->rowCount();
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+        if($row > 0){
+            $new_quantity = $results['quantity'] + 1;
+            $req = "UPDATE carts SET quantity = :new_quantity WHERE id_user = :id_user AND id_item = :id_item ";
+            $stmt = Database::connect_db()->prepare($req);
+            $stmt->execute(array(
+                ":id_user" => $id_user,
+                ":id_item" => $id_item,
+                ":new_quantity" => $new_quantity
+            ));
+            header("Location: cart.php");
+            exit();
         }
-        else{
-            $req = "INSERT INTO carts (id_user, id_item) VALUE (:id_user, :id_item)";
+        elseif($row == 0){
+            $req = "INSERT INTO carts (id_user, id_item, quantity) VALUE (:id_user, :id_item, 1)";
             $stmt = Database::connect_db()->prepare($req);
             $stmt->execute(array(
                 ':id_user' => $id_user,
@@ -29,5 +37,6 @@ class Cart_model{
             exit();
         }
     }
+
 }
 ?>
